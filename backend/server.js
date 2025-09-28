519b5ab34a6d38db7bfd89519299ac3824db94cf
godumapraneeth <161477487+godumapraneeth@users.noreply.github.com>
2025-09-28 12:38:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ import chatRoutes from "./routes/chatRoutes.js";
 connectDB();
 
 const corsOptions={
-  origin:https://project-collaboration.vercel.app,
+  origin:"https://project-collaboration.vercel.app",
   credentials:true,
   optionsSuccessStatus:200
 }
@@ -29,7 +29,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { 
   cors: {
-     origin: https://project-collaboration.vercel.app,
+     origin: "https://project-collaboration.vercel.app",
      methods:["GET","POST"],
      credentials:true,    
     } });
